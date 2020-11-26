75bb9a9f1b5bc1f6e6a9e1c5e24328d5b9741e0b
LAPTOP-1U1SBC38\ferna <ferbert15@gmail.com>
2020-11-26 18:01:24 +0100

    *chat works!!

--- src/middlewares/check.Jwt.ts
+++ src/middlewares/check.Jwt.ts
@@ -2,22 +2,22 @@ import { Request, Response, NextFunction  } from 'express';
 import jwt from 'jsonwebtoken';
 import config from '../config/config.json';
 
-export const checkJwt = (req:Request, res: Response, next: NextFunction) => {
+export const checkJwt = (roleToCheck: String) => {
+    return (req:Request, res: Response, next: NextFunction) => {
+        const token = <string> req.headers["authorization"];
 
-    const token = <string> req.headers["authorization"];
+        try {
+            // devuelve el objeto del token
+            jwt.verify(token, config.jwtSecret);
+        
+            // throw new Error("Dont have permmissions")
+            // res 4040
+        } catch (error) {
 
-    try {
+            res.sendStatus(401);
+        }
 
-        jwt.verify(token, config.jwtSecret);
-       
-
-
-    } catch (error) {
-
-        res.sendStatus(401);
+        next();
     }
-
-    next();
-
 }
 
